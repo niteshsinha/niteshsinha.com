4f8b13f758a61b03cd90280ce05785178c9121c6
Nitesh Sinha <niteshsinha.mail@gmail.com>
2020-08-17 15:11:09 +0100

    disable thylameaf

--- service/src/main/java/com/niteshsinha/candid/service/CandidService.java
+++ service/src/main/java/com/niteshsinha/candid/service/CandidService.java
@@ -8,11 +8,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Slf4j
-@Controller
+@RestController
 public class CandidService {
 
     private final CandidRepository candidRepository;
